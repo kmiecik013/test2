fe8518f1fce3597899fa966488348f707be8206a
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-10-08 17:16:12 -0600

    addition of footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Taylor from './components/Taylor';
 import NavBarThree from './components/NavBarThree';
 import Modal from './components/Modal';
 import Acknowledgement from './components/Acknowledgement';
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
       </div>
 
       <Acknowledgement />
+      <Footer />
     </div>
   );
 }

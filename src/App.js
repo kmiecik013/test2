6835bacabb3f71c720d3a8a922ab34335edb2f14
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-10-08 16:03:34 -0600

    added tayler

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
 import Home from './components/Home';
+import Taylor from './components/Taylor';
 
 function App() {
   return (
     <div>
     <Home />
-    <About />
+    <Taylor />
     </div>
   );
 }

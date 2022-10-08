b9462e0c9d9398dd313557239e85cacd0560d634
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-10-08 16:56:02 -0600

    addition of gethelpnow button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
+import {useState} from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
 import Home from './components/Home';
 import Taylor from './components/Taylor';
 import NavBarThree from './components/NavBarThree';
+import Modal from './components/Modal';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div>
       <NavBarThree />
     <About />
     <Taylor />
+
+    <div className="gethelp-postioning">
+
+    <button className="gethelp-button"
+            onClick= {()=>setIsOpen(true)}>Get Help Now</button>
+       <Modal open={isOpen} onClose={()=> setIsOpen(false)}>
+        </Modal>
+      </div>
     </div>
   );
 }
--- src/components/TaylorTitle.jsx
+++ src/components/TaylorTitle.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+
 import '../styles/taylortitle.css'
 
 

beacad5019ec7e9c96a93a6b696a6577e1563813
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-10-08 15:49:20 -0600

    added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
+import Home from './components/Home';
 
 function App() {
   return (
+    <div>
+    <Home />
     <About />
+    </div>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+
+import "../styles/home.css";
+import SCtitle from './SCtitle'
+
+
+
+export default function Home() {
+  return (
+    <div className="home-page" id="home">
+      <div className="fade-in-text">
+        <p>“I breathe in calmness, I breathe out stress”</p>
+      </div>
+        <SCtitle />
+    </div>
+  );
+}

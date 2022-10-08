b89ea3bfdc6de3f76cd4a99731b124b0243c8c04
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-10-08 16:19:58 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import About from './components/About';
 import Home from './components/Home';
 import Taylor from './components/Taylor';
+import NavBarThree from './components/NavBarThree';
+
 
 function App() {
   return (
     <div>
+      <NavBarThree />
     <About />
     <Taylor />
     </div>

bbdcb1960d9edd99a7d117a3960242868b166cce
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-10-19 16:22:02 -0600

    added stuff to get help now

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -40,15 +40,15 @@ export default function Modal({ open, children, onClose }) {
         <div className="distres-box">
         <div className="crisis-option">
           <h1 className="distress-title">Distress Center</h1>
-          <a className="distress-number" href="tel:14165709210"> 1-403-266-1601</a>
+          <span className="distress-number"><a  href="tel:14165709210"> 1-403-266-1601</a></span>
           </div>
           <div className="crisis-option">
           <h1 className="distress-title">Hope For Wellness Help</h1>
-          <a className="distress-number" href="tel:18552423310"> 1-855-242-3310</a>
+          <span className="distress-number"><a href="tel:18552423310"> 1-855-242-3310</a></span>
           </div>
           <div className="crisis-option">
           <h1 className="distress-title">National Indian Residential School Crisis</h1>
-          <a className="distress-number" href="tel:18669254419"> 1-866-925-4419</a>
+          <span className="distress-number"><a href="tel:18669254419"> 1-866-925-4419</a></span>
           </div>
         </div>
         {children}

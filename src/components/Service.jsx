1d573e90445ffcc8cef128bdc04469dd57278f8b
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-10-26 17:12:06 -0600

    added more stuff

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -47,7 +47,7 @@ return (
   
     <div className="slider-part">
 
- 
+  <div className="carousel1">
     <Carousel
    additionalTransfrom={0}
 arrows
@@ -131,6 +131,51 @@ arrows
       <CardId4 />
         <CardId5 />
         </Carousel>
+        </div>
+<div className="carousel2">
+<Carousel
+  additionalTransfrom={0}
+  arrows
+  autoPlaySpeed={3000}
+  centerMode={false}
+  className=""
+  containerClass="container"
+  dotListClass=""
+  draggable
+  focusOnSelect={false}
+  infinite
+  itemClass=""
+  keyBoardControl
+  minimumTouchDrag={80}
+  pauseOnHover
+  renderArrowsWhenDisabled={false}
+  renderButtonGroupOutside={false}
+  renderDotsOutside={false}
+  responsive={{
+    mobile: {
+      breakpoint: {
+        max: 425,
+        min: 0
+      },
+      items: 1
+    }
+
+  }}
+  rewind={false}
+  rewindWithAnimation={false}
+  rtl={false}
+  shouldResetAutoplay
+  showDots
+  sliderClass=""
+  slidesToSlide={1}
+  swipeable
+><CardId1 />
+  <CardId2 />
+    <CardId3 />
+      <CardId4 />
+        <CardId5 />
+        </Carousel>
+        </div>
   </div>
 
  </div>

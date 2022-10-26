461df2026a289babbe9d91a7037e2affbbd47470
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-10-26 15:44:33 -0600

    added more adjustments

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,21 @@
 
 import "../styles/home.css";
 import SCtitle from './SCtitle';
-import Sage from './Sage';
+import Sage2 from './Sage2';
 
 
 
 export default function Home() {
   return (
+    <div>
     <div className="home-page" id="home">
       <div className="fade-in-text">
         <p>“I breathe in calmness, I breathe out stress”</p>
       </div>
         <SCtitle />
-        <Sage />
+       
+        </div> 
+        <Sage2 />
     </div>
   );
 }
--- src/components/Sage2.jsx
+++ src/components/Sage2.jsx
@@ -2,23 +2,23 @@ import Leaf from "./Leaf"
 import Leaf2 from "./Leaf2"
 import SC from "./SC"
 import Dots from "./Dots"
-import '../styles/sage.css'
+import '../styles/sage2.css'
 import { ImOpt } from "react-icons/im"
 
 
-export default function Sage() {
+export default function Sage2() {
 
 
   return (
-    <div className="between-tags">
+    <div className="home-sage">
      
-    <Sage />
+    <Dots />
     <Leaf />
     <Leaf />
     <SC />
     <Leaf2 />
     <Leaf2 />
-    <Sage />
+    <Dots />
     
     </div>
     

7be24e24f4f7e69abe51b84195e956a0016a903a
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-11-07 14:59:09 -0700

    additon of more

--- src/components/CardsId/CardId5.jsx
+++ src/components/CardsId/CardId5.jsx
@@ -27,7 +27,7 @@ export default function CardId5(props) {
       </div>
       <div className="card-content">
       <div >
-        <h2 className="title">Individual2fewkfnewfkfnefnekfkfenlwfewmlfewmpewejffkejffeffefee</h2>
+        <h2 className="title">Individual2fewkfnewfkfnefnekfkfenlwfewmlfewmpewkwenflneejffkejffeffefee</h2>
       </div>
       <div className="description"> 
     

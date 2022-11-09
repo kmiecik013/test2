7f88ce181e6f572c2e7ba880854ac67c4fce03bd
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-11-08 21:26:13 -0700

    modified a small change

--- src/components/CardsId/CardId2.jsx
+++ src/components/CardsId/CardId2.jsx
@@ -38,7 +38,7 @@ export default function CardId1(props) {
             class={props.title}
             key={props.id}
             id= {props.id}
-            onClick= {handleClick} >More Infokdnkfef</button>
+            onClick= {handleClick} >More Infokdnkfeffefef</button>
        <ModalId2
        open={isOpen} onClose={()=> setIsOpen(false)}>
         </ModalId2>

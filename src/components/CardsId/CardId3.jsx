3b5c5ee9c645920e8c58d470fdbb8376cbe1350d
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-11-25 23:43:27 -0700

    additon of new title

--- src/components/CardsId/CardId3.jsx
+++ src/components/CardsId/CardId3.jsx
@@ -29,7 +29,7 @@ export default function CardId1(props) {
       </div>
       <div className="card-content">
       <div>
-        <h2 className="title atw">Art Therapy Wvvdveevjnfnorkshops</h2>
+        <h2 className="title atw">Art Therapy Wvvdveevjnfncnwjnwcorkshops</h2>
       </div>
       <div className="description"> 
       

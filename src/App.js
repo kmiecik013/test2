c018c46670347398e79e7d94dceea369c5051128
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-12-21 21:39:13 -0700

    mods for the main page

--- src/App.js
+++ src/App.js
@@ -1,90 +1,44 @@
-import {useState} from 'react'
-import './App.css';
-import About from './components/About';
-import Home from './components/Home';
-import Taylor from './components/Taylor';
-import NavBarThree from './components/NavBarThree';
-import Modal from './components/Modal';
-import Acknowledgement from './components/Acknowledgement';
-import Footer from './components/Footer';
-import Contact from './components/Contact';
-import Service from './components/Service';
-import BacktoTop from './components/BacktoTop';
-
-
-
+import { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Taylor from "./components/Taylor";
+import NavBarThree from "./components/NavBarThree";
+import Modal from "./components/Modal";
+import Acknowledgement from "./components/Acknowledgement";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Service from "./components/Service";
+import BacktoTop from "./components/BacktoTop";
 
 function App() {
-
   const [isOpen, setIsOpen] = useState(false);
 
   return (
+    <div>
+      <div className="gethelp-postioning">
+        <button className="gethelp-button" onClick={() => setIsOpen(true)}>
+          Get Help Now
+        </button>
+        <Modal open={isOpen} onClose={() => setIsOpen(false)}></Modal>
+      </div>
 
+      <div className="wrapper">
+        <div className="top">
+          <NavBarThree />
+          <Home />
+        </div>
 
-<div>
-  
-  <BacktoTop />
-
- <div className="gethelp-postioning">
-
-    <button className="gethelp-button"
-            onClick= {()=>setIsOpen(true)}>Get Help Now</button>
-       <Modal open={isOpen} onClose={()=> setIsOpen(false)}>
-        </Modal>
-
-
-      </div> 
- 
-
-   <div className="wrapper">
-
- 
-
-
-
- 
-
-   
-
-  <div className="top"> 
-  
-
-      <NavBarThree />
-
-   <Home />
-
-  </div>
-
-      
-    
-    
-
-  <div className="bottom">
-    
-    <About/>
-    
-    
-    <Taylor />
-
-
-     
-
-      <Service />
-      <Contact />
-    
-
-
-      <Acknowledgement />
-      <Footer />
-      
-
-   </div>
-</div>
-
-
-
-</div>
-  
+        <div className="bottom">
+          <About />
+          <Taylor />
+          <Service />
+          <Contact />
+          <Acknowledgement />
+          <Footer />
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,77 +2,57 @@ import art from "../images/art.jpeg";
 import art2 from "../images/newsage.jpg";
 import Card from "react-bootstrap/Card";
 import art3 from "../images/art3.jpg";
-import Sage from './Sage';
+import Sage from "./Sage";
 import "../styles/about.css";
 import { GiLindenLeaf } from "react-icons/gi";
 import { IoLeafOutline } from "react-icons/io5";
-import { Parallax } from 'react-parallax'
+import { Parallax } from "react-parallax";
 
 export default function About() {
   return (
+    <div className="about-section" id="about">
+      <Sage />
 
-
-    <div className="about-section"id="about">
-
-<Sage />
-     
       <div className="about-page-structure">
-
         <div className="title-strcuture">
-      
-     <h2 className="art-subtitle">What is Art Therapy?</h2>
+          <h2 className="art-subtitle">What is Art Therapy?</h2>
 
-     <img className="mobile-pic" src={art3} />
+          <img className="mobile-pic" src={art3} />
 
-     
-
-        
-        
-
-            <div className="text-on-left">
-          <p className="art-therapy-description atd1">
+          <div className="text-on-left">
+            <p className="art-therapy-description atd1">
               Art Therapy is a therapeutic modality that uses the creative
               process of art making as a vehicle to improvement in ones
               physical, emotional, spiritual, and mental health. The process of
               creating art and reflecting on themes and metaphors with the help
               of the art therapist can bring forth inner thoughts and feelings
-              that may otherwise be difficult to articulate in words. </p>
-              <p className="art-therapy-description atd2">You do not
-              have to be an artist to benefit from art therapy. In art therapy
-              the emphasis is based on the process and self-expression, not the
-              final product. Art therapy also aligns with the belief that
-              everyone is innately creative.</p>
+              that may otherwise be difficult to articulate in words.{" "}
+            </p>
+            <p className="art-therapy-description atd2">
+              You do not have to be an artist to benefit from art therapy. In
+              art therapy the emphasis is based on the process and
+              self-expression, not the final product. Art therapy also aligns
+              with the belief that everyone is innately creative.
+            </p>
 
-              <div className="green-leaves">
-          <IoLeafOutline 
-            className="single-green-leaf-about"></IoLeafOutline>
-            <IoLeafOutline 
-            className="single-green-leaf-about"></IoLeafOutline>
-           <IoLeafOutline 
-            className="single-green-leaf-about"></IoLeafOutline>
+            <div className="green-leaves">
+              <IoLeafOutline className="single-green-leaf-about"></IoLeafOutline>
+              <IoLeafOutline className="single-green-leaf-about"></IoLeafOutline>
+              <IoLeafOutline className="single-green-leaf-about"></IoLeafOutline>
             </div>
-            
-            </div>
-            </div>
-            <div>
+          </div>
+        </div>
+        <div>
           <img className="sage-img" src={art} />
           <div>
             <div className="bottom-image">
-            <img className="sage-img2" src={art2} />
+              <img className="sage-img2" src={art2} />
             </div>
-            
-
           </div>
-          </div>
-          
-       
+        </div>
       </div>
 
       <Sage />
-
-
-    
     </div>
-   
   );
 }

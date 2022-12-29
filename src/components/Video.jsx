54374d37022ca3c1933d5e4f67cc7df73121af01
MIchael  Kmiecik <93628363+kmiecik013@users.noreply.github.com>
2022-12-29 00:34:23 -0700

    addition of video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -6,12 +6,15 @@ export default function Video() {
 
   return (
     <div>
-      <video src={video} 
+      <video
         type="video/mp4"
-        autoPlay 
-        loop
-        muted
-        className="video-source"/>
+        autoPlay={true} 
+        loop={true}
+        muted={true}
+        playsincline={true}
+        className="video-source">
+          <source src={video}/>
+          </video>
  
     </div>
   )
